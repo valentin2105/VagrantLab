c0666321756642d96597b7109d7996deab238132
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2023-07-24 20:06:19 +1100

    app

--- k8s/demoapp/app.py
+++ k8s/demoapp/app.py
@@ -8,7 +8,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello from k8s ! I have been seen {} times.\n'.format(count)
 
-    if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8000, debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
