916503170938698fd1a9c93462bff3e2c66e6d96
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2023-07-25 07:48:38 +1100

    DemoApp

--- k8s/demoapp/app.py
+++ k8s/demoapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from redis import Redis
 
 app = Flask(__name__)
@@ -8,7 +8,8 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello from k8s ! I have been seen {} times.\n'.format(count)
+    return render_template('index.html', visits=count)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
