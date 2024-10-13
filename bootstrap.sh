9ef8fed5f29b1b9f241dad1a438f784b1a8ef929
Valentin Ouvrard <valentin210594@gmail.com>
2024-10-14 08:39:41 +1100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,18 +8,18 @@ sudo apt-get -y dist-upgrade
 
 sudo systemctl start iscsid
 
-#sudo install -m 0755 -d /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#sudo chmod a+r /etc/apt/keyrings/docker.gpg
-#echo \
-#  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-#  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-#  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 
-#sudo apt-get update
-#sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  || exit 1
-#sudo docker ps
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin  || exit 1
+sudo docker ps
 
 mkdir -p /etc/rancher/k3s
 
@@ -32,9 +32,8 @@ EOF
 
 sudo apt-get -y autoremove
 
-#wget https://github.com/k3s-io/k3s/releases/download/v1.28.7%2Bk3s1/k3s -q --show-progress
-#chmod +x k3s && sudo mv k3s /usr/local/bin/ 
+wget https://github.com/k3s-io/k3s/releases/download/v1.30.5%2Bk3s1/k3s -q --show-progress
+chmod +x k3s && sudo mv k3s /usr/local/bin/ 
 
 echo "nameserver 8.8.8.8" | sudo tee    /etc/resolv.conf
 echo "nameserver 1.1.1.1" | sudo tee -a /etc/resolv.conf
-
